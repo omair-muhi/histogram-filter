d639f86f2e9c103753c2530b069d57448cbfa86f
Omair Muhi <omairmuhi@gmail.com>
2019-09-07 11:01:35 -0400

    SUMMARY: -Ensure new_beliefs is a 2D array -Add code to normalize DESCRIPTION: TESTING: -Verified with test-inputs OTHER:

--- sense.py
+++ sense.py
@@ -10,11 +10,26 @@ def sense(color, grid, beliefs, p_hit, p_miss):
     new_beliefs = []
     # 1) Iterate over 2D-grid
     for row in range(len(grid)):
+        new_beliefs_tmp = []
         for col in range(len(grid[row])):
+            # update individual cells with new probability
+            # based on observation made
             if grid[row][col] == color:
-                # update the cell with the observed colour
-                new_beliefs.append(beliefs[row][col] * p_hit)
+                # create a sub-list
+                new_beliefs_tmp.append(beliefs[row][col] * p_hit)
             else:
-                new_beliefs.append(beliefs[row][col] * p_miss)
+                new_beliefs_tmp.append(beliefs[row][col] * p_miss)
+        # add sub-list to main list
+        new_beliefs.append(new_beliefs_tmp)
     # 2) Normalize the updated grid - TODO
+    sum_tmp = []
+    for nested_list in new_beliefs:
+        # sum all nested-lists
+        sum_tmp.append(sum(nested_list))
+    # collapse the nested-lists into total sum
+    normalized_sum = sum(sum_tmp)
+    for i in range(len(new_beliefs)):
+        for j in range(len(new_beliefs[i])):
+    #         # normalize
+            new_beliefs[i][j] = new_beliefs[i][j]/normalized_sum
     return new_beliefs
