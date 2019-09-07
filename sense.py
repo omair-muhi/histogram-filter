7bed13fea08d43d7cf6f55953f19017794a728a7
Omair Muhi <omairmuhi@gmail.com>
2019-09-07 08:48:54 -0400

    SUMMARY: Initial template code for 2D localizer sense() routine DETAILED-DESCRIPTION: N/A TESTING: N/A OTHER: N/A

---
+++ sense.py
@@ -0,0 +1,16 @@
+# DESCRIPTION: Sense function for a robot-localizer
+# PARAMETERS:
+# 1) color: the observation that was just made
+# 2) grid: a 2D-grid representation of the world
+# 3) beliefs: a probability-distribution of initial new_beliefs
+# 4) p_hit: probability of making a correct observation of 'color'
+# 5) p_miss: probability of making an incorrect observation of 'color'
+# RETURNS: updated probability-distribution of beliefs
+def sense(color, grid, beliefs, p_hit, p_miss):
+    new_beliefs = []
+
+    #
+    # TODO - implement this in part 2
+    #
+
+    return new_beliefs
